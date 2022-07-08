0ac4e7419a894ce522ba974628fb958c0c6cd60d
59417 <sun313131@gmail.com>
2022-07-08 12:53:23 +0800

    Edit About me

--- components/body/About.js
+++ components/body/About.js
@@ -15,8 +15,10 @@ export default function About() {
             <div className={classes.about_content}>
                 <h2>About Me</h2>
                 <ul>
+                    <p>Hi! I&#39;m Yi Ching &#58;&#41;</p>
+                    <li>從 0 開始自學 &#62; 2個月後用 NextJs 獨立完成第一個網站</li>
                     <li>2022/05~ 前端接案 &#38; 軟體公司實習</li>
-                    <li>2022/02~ 自學前端 &#40;React &#38; Next.js&#41;</li>
+                    <li>2022/02~ 自學前端 &#40;React &#38; NextJs&#41;</li>
                     <li>2021/11~2022/04 數據分析接案 &#40;Python &#38; R&#41;</li>
                     <li>2021/06~2021/10 AI大數據企業實務應用班 &#40;Python &#38; R&#41;</li>
                     <li>AWS Cloud Practitioner 證照</li>
